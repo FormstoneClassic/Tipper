0aed44ebd604f70f21a119427d9d9e3e9543922d
Ben Plum <mr@benplum.com>
2012-11-24 14:15:19 -0500

    Moving project from Tooltip to Formstone Library

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Tipper
-======
+========
 
-A jQuery plugin for attaching custom tooltips. 
+A jQuery plugin for attaching custom tooltips to elements
+
+[Documentation and Examples](http://www.benplum.com/formstone/tipper/)
---
+++ jquery.fs.tipper.js
@@ -0,0 +1,135 @@
+/*
+ * Tipper Plugin [Formstone Library]
+ * @author Ben Plum
+ * @version 0.4
+ *
+ * Copyright © 2012 Ben Plum <mr@benplum.com>
+ * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
+ */
+
+if (jQuery) (function($) {
+	
+	var options = {
+		direction: "right",
+		follow: false,
+		formatter: function() {},
+		margin: 15
+	};
+	
+	// Public Methods
+	var pub = {
+		
+		defaults: function(opts) {
+			options = $.extend(options, opts || {});
+			return $(this);
+		},
+		
+		destroy: function() {
+			return $(this).off(".tipper")
+					  	  .data("tipper", null);
+		}
+	};
+		
+	function _init(opts) {
+		options.formatter = _format;
+		return $(this).on("mouseenter.tipper", _build)
+					  .data("tipper", $.extend({}, options, opts || {}));
+	}
+	
+	function _build(e) {
+		var $target = $(this);
+		var data = $target.data("tipper");
+		var html = '<div class="tipper-wrapper"><div class="tipper-content">';
+		html += data.formatter.apply($("body"), [$target]);
+		html += '</div><span class="tipper-caret"></span></div>';
+		
+		$target.data("tipper-text", $target.attr("title")).attr("title", null);
+		
+		var $tipper = $('<div class="tipper-positioner" />');
+		$tipper.append(html).appendTo("body");
+		
+		var $caret = $tipper.find(".tipper-caret");
+		var offset = $target.offset();
+		var targetWidth = $target.outerWidth();
+		var targetHeight = $target.outerHeight();
+		var tipperWidth = $tipper.outerWidth(true);
+		var tipperHeight = $tipper.outerHeight(true);
+		
+		var classes = data.direction;
+		var tipperPos = {};
+		var caretPos = {};
+		
+		if (data.direction == "right" || data.direction == "left") {
+			tipperPos.top = offset.top - ((tipperHeight - targetHeight) / 2);
+			caretPos.top = (tipperHeight - $caret.outerHeight()) / 2;
+			
+			if (data.direction == "right") {
+				tipperPos.left = offset.left + targetWidth + data.margin;
+			} else if (data.direction == "left") {
+				tipperPos.left = offset.left - tipperWidth - data.margin;
+			}
+		} else {
+			classes += ' vertical';
+			tipperPos.left = offset.left - ((tipperWidth - targetWidth) / 2);
+			caretPos.left = (tipperWidth - $caret.outerWidth()) / 2;
+			
+			if (data.direction == "bottom") {
+				tipperPos.top = offset.top + targetHeight + data.margin;
+			} else if (data.direction == "top") {
+				tipperPos.top = offset.top - tipperHeight - data.margin;
+			}
+		}
+		
+		$tipper.css(tipperPos)
+				.addClass(classes);
+		$caret.css(caretPos);
+		
+		$target.one("mouseleave.tipper", { 
+			$tipper: $tipper,
+			$target: $target
+		}, _destroy);
+	}
+	
+	function _format($target) {
+		return $target.attr("title");
+	}
+	
+	function _destroy(e) {
+		var data = e.data;
+		data.$target.attr("title", data.$target.data("tipper-text")).data("tipper-text", null);
+		data.$tipper.remove();
+	}
+	
+	/*
+	function _move(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		
+		var $tipper = $("#tipper");
+		
+		var width = $tipper.outerWidth(true);
+		var height = $tipper.outerHeight(true);
+		
+		var left = e.pageX + options.cursorOffset;
+		var top = e.pageY - ((options.caretOffset === "center") ? (height / 2) : options.caretOffset);
+		
+		if (left + width > $(window).width()) {
+			$tipper.addClass("left");
+			left = e.pageX - width - options.cursorOffset;
+		} else {
+			$tipper.removeClass("left");
+		}
+		
+		$tipper.css({ left: left, top: top });
+	}
+	*/
+	
+	$.fn.tipper = function(method) {
+		if (pub[method]) {
+			return pub[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || !method) {
+			return _init.apply(this, arguments);
+		}
+		return this;
+	};
+})(jQuery);
